37a3d8dda75830326abfb724aebb8d96a9b4bf4f
8coon <8coon@users.noreply.github.com>
2015-07-04 21:35:11 +0300

    Create list.lua

---
+++ update/list.lua
@@ -0,0 +1,19 @@
+local list = {
+	updates = {},
+
+
+	software = {},
+
+
+	thirdparty = {
+		{
+			title = "TestPackage",
+			version = "1.0.0",
+			author = "8coon",
+			description = "Ever since I released the first version of ComputerCraft back in 2011, I’ve always been amazed and impressed by the number of people who’ve come up to me to tell me they’ve used ComputerCraft to teach themselves programming. Even more impressive is the number of school teachers and educators who’ve taken it upon themselves to use the mod to teach programming to others. Today, I’m announcing the release of a new project that takes the educational potential of ComputerCraft to a whole new level.",
+			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
+			minVersion = "1.0.0",
+			downloadLink = "",
+		},
+	},
+}
