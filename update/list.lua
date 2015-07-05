b2e8c825fd68b1a897f72c2f6cbe2759cf637212
8coon <8coon@users.noreply.github.com>
2015-07-05 10:21:19 +0300

    Update list.lua

--- update/list.lua
+++ update/list.lua
@@ -13,7 +13,7 @@ local list = {
 			description = "Ever since I released the first version of ComputerCraft back in 2011, I've always been amazed and impressed by the number of people who've come up to me to tell me they've used ComputerCraft to teach themselves programming.",
 			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
 			minVersion = "1.0.0",
-			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/list.lua",
+			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/test.wpk",
 		},
 
 		{
@@ -23,7 +23,7 @@ local list = {
 			description = "Ever since I released the first version of ComputerCraft back in 2011, I've always been amazed and impressed by the number of people who've come up to me to tell me they've used ComputerCraft to teach themselves programming.",
 			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
 			minVersion = "1.0.0",
-			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/list.lua",
+			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/test.wpk",
 		},
 
 		{
@@ -33,7 +33,7 @@ local list = {
 			description = "Ever since I released the first version of ComputerCraft back in 2011, I've always been amazed and impressed by the number of people who've come up to me to tell me they've used ComputerCraft to teach themselves programming.",
 			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
 			minVersion = "1.0.0",
-			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/list.lua",
+			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/test.wpk",
 		},
 
 		{
@@ -43,7 +43,7 @@ local list = {
 			description = "Ever since I released the first version of ComputerCraft back in 2011, I've always been amazed and impressed by the number of people who've come up to me to tell me they've used ComputerCraft to teach themselves programming.",
 			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
 			minVersion = "1.0.0",
-			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/list.lua",
+			downloadLink = "https://raw.githubusercontent.com/8coon/ccwin/dev/update/test.wpk",
 		},
 	},
 }
