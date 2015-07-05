7e2a7d835e99843ec1244a3a3002e079fed3e884
8coon <8coon@users.noreply.github.com>
2015-07-05 03:19:39 +0300

    Update list.lua

--- update/list.lua
+++ update/list.lua
@@ -25,6 +25,26 @@ local list = {
 			minVersion = "1.0.0",
 			downloadLink = "",
 		},
+
+		{
+			title = "TestPackage3",
+			version = "1.0.0",
+			author = "8coon",
+			description = "Ever since I released the first version of ComputerCraft back in 2011, I've always been amazed and impressed by the number of people who've come up to me to tell me they've used ComputerCraft to teach themselves programming.",
+			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
+			minVersion = "1.0.0",
+			downloadLink = "",
+		},
+
+		{
+			title = "TestPackage4",
+			version = "1.0.0",
+			author = "8coon",
+			description = "Ever since I released the first version of ComputerCraft back in 2011, I've always been amazed and impressed by the number of people who've come up to me to tell me they've used ComputerCraft to teach themselves programming.",
+			icon = "pic|3|4|b0*b0 b0 b0 b0*b0 b0 b0 87=88=88=8d*",
+			minVersion = "1.0.0",
+			downloadLink = "",
+		},
 	},
 }
 
